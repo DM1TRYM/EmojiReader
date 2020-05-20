3db1574d4bac1e99eba7cc10d7ddc94ae4c31ccf
Dmitrii <dm1trym2130@gmail.com>
2020-05-20 15:28:21 +0300

    Add files via upload

---
+++ EmojiReader/EmojiReader/EmojiModel.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Mikhailov on 29.04.2020.
+//  Copyright © 2020 Funtik Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
+
